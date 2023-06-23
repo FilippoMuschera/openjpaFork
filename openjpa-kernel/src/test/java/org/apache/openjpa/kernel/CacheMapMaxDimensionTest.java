2cfab03b1d2d46a3990161f3617d656d1744ca55
Filippo Muschera <filippo.muschera@students.unroma2.eu>
2023-06-23 19:55:48 +0200

    Minor improvements

--- openjpa-kernel/src/test/java/org/apache/openjpa/kernel/CacheMapMaxDimensionTest.java
+++ openjpa-kernel/src/test/java/org/apache/openjpa/kernel/CacheMapMaxDimensionTest.java
@@ -1,3 +1,22 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 package org.apache.openjpa.kernel;
 
 import org.apache.openjpa.util.CacheMap;
@@ -68,15 +87,18 @@ public class CacheMapMaxDimensionTest {
         //nella softMap. Ci aspettiamo anche che la pinnedMap sia sempre vuota dal momento che non eseguiamo nessuna
         //operazione di pin sulla cacheMap.
         cacheMap.put(2,2);
-        System.out.printf("Main cache size: %d, soft map size: %d, total size:%d\n", cacheMap.getMainCacheSize(), cacheMap.getSoftMapSize(), cacheMap.getTotalSize());
+        System.out.printf("Main cache size: %d, soft map size: %d, total size:%d\n",
+                cacheMap.getMainCacheSize(), cacheMap.getSoftMapSize(), cacheMap.getTotalSize());
         assertTrue(cacheMap.getMainCacheSize() == max && cacheMap.getSoftMapSize() == cacheMap.getTotalSize() - cacheMap.getMainCacheSize());
 
         cacheMap.put(3,3);
-        System.out.printf("Main cache size: %d, soft map size: %d, total size:%d\n", cacheMap.getMainCacheSize(), cacheMap.getSoftMapSize(), cacheMap.getTotalSize());
+        System.out.printf("Main cache size: %d, soft map size: %d, total size:%d\n",
+                cacheMap.getMainCacheSize(), cacheMap.getSoftMapSize(), cacheMap.getTotalSize());
         assertTrue(cacheMap.getMainCacheSize() == max && cacheMap.getSoftMapSize() == cacheMap.getTotalSize() - cacheMap.getMainCacheSize());
 
         cacheMap.put(4,4);
-        System.out.printf("Main cache size: %d, soft map size: %d, total size:%d\n", cacheMap.getMainCacheSize(), cacheMap.getSoftMapSize(), cacheMap.getTotalSize());
+        System.out.printf("Main cache size: %d, soft map size: %d, total size:%d\n",
+                cacheMap.getMainCacheSize(), cacheMap.getSoftMapSize(), cacheMap.getTotalSize());
         assertTrue(cacheMap.getMainCacheSize() == max && cacheMap.getSoftMapSize() == cacheMap.getTotalSize() - cacheMap.getMainCacheSize());
 
 
